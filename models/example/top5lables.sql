c04b2c1e0da26ee1d451a3f9ed9f7942a52dc6a6
meenegerreddy-afk <meenege.r.reddy@kipi.ai>
2026-05-03 15:32:48 +0530

    deployment1

---
+++ models/example/top5lables.sql
@@ -0,0 +1,6 @@
+SELECT l.name AS label_name, COUNT(ml.message_id) AS message_count
+FROM {{ source('fivetran', 'MESSAGE_LABEL') }} ml
+JOIN {{ source('fivetran', 'LABEL') }} l ON ml.label_id = l.id
+GROUP BY l.name
+ORDER BY message_count DESC
+LIMIT 5;
